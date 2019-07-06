abfa0bfd98dfd8d218d836f13cf762d0f4291aa5
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-07-06 20:52:25 +0530

    Updated method information.

--- sql_tools/sqlite/__init__.py
+++ sql_tools/sqlite/__init__.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite
 
 from numpy import array
 
@@ -9,17 +9,17 @@ from .databFuncs import *
 from .execute import execute
 
 __version__ = "SQL Tools version 0.1.9"
-__sqliteVersion__ = f"SQL Tools: Sqlite3 version {sqlite3.sqlite_version}"
-__sqliteFunctions__ = array(["Create database: Sqlite3.createDatabase()",
-                             "Move database: Sqlite3.moveDatabase()",
-                             "Copy database: Sqlite3.copyDatabase()",
-                             "Delete database: Sqlite3.delDatabase()",
-                             "Get no. of records in table(s): Sqlite3.getNoOfRecords()",
-                             "Get no. of columns in table(s): Sqlite3.getNoOfColumns()",
-                             "Get no. of table(s) in database(s): Sqlite3.getNoOfTables()",
-                             "Get columns names of table(s): Sqlite3.getColumnNames()",
-                             "Get table names of database(s): Sqlite3.getTableNames()",
-                             "Get table(s) creation command: Sqlite3.getTableCommand()"])
+__sqliteVersion__ = f"SQL Tools: sqlite version {sqlite.sqlite_version}"
+__sqliteFunctions__ = array(["Create database: sqlite.createDatabase()",
+                             "Move database: sqlite.moveDatabase()",
+                             "Copy database: sqlite.copyDatabase()",
+                             "Delete database: sqlite.delDatabase()",
+                             "Get no. of records in table(s): sqlite.getNoOfRecords()",
+                             "Get no. of columns in table(s): sqlite.getNoOfColumns()",
+                             "Get no. of table(s) in database(s): sqlite.getNoOfTables()",
+                             "Get columns names of table(s): sqlite.getColumnNames()",
+                             "Get table names of database(s): sqlite.getTableNames()",
+                             "Get table(s) creation command: sqlite.getTableCommand()"])
 __help__ = "Visit the documentation for more help or type \"help(sql_tools)\""
 
     
